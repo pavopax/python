9dd58861c1b7a70966ebf9195d4e7ad84efb4b89
Paul Paczuski <pavopax@users.noreply.github.com>
2020-04-18 13:37:05 -0400

    Add modules/robust_evaluate.py (#1)
    * Add first module: robust_evaluate.py
    
    * Docs: fix typos
    
    * Major updates to robust_evaluate.py
    
    * Simplify robust_evaluate and add main.py app
    
    * Docs: update examples

---
+++ modules/README.md
@@ -0,0 +1,84 @@
+# Modules for ML (Working)
+
+
+  * `robust_evaluate.py`
+  
+	**A quick way to robustly evaluate signal in a classification task.**
+	  
+	Given X, y, this script runs a Logistic Regression and evaluates it with
+	nested cross-validation.
+
+	Useful for smaller data (less than thousands of records) and potentially
+	lots of features, to quickly identify if there is a signal.
+
+    **Usage**
+
+
+	```python
+	from robust_evaluate import Evaluator
+	
+    est = LogisticRegressionCV()
+	
+	ev = Evaluator(est, n_repeats=3, n_jobs=2)
+	ev.train_evaluate(X, y)
+	print(ev.scores_)
+
+	# if you have additional holdout data
+	ev.train_predict(X, y, X_holdout, y_holdout)
+	print(ev.holdout_scores_)
+	```
+
+    **Example with synthetic data**
+	
+	Usage: `python robust_evaluate.py` ,add `--help` for details
+	 
+
+    ```bash
+	modules$ python robust_evaluate.py --imbalanced
+	Creating data...
+	Evaluating...
+	                         mean    std  n_scores
+	test_roc_auc            0.963  0.036       5.0
+	test_average_precision  0.800  0.170       5.0
+	test_neg_log_loss      -0.091  0.028       5.0
+	test_accuracy           0.965  0.012       5.0
+	test_recall             0.300  0.245       5.0
+	test_precision          0.600  0.490       5.0
+	test_f1                 0.400  0.327       5.0
+	test_f1_weighted        0.953  0.022       5.0
+	```
+
+
+  * `main.py`
+
+	**More experiments using `robust_evaluate.py`**
+  
+	Example
+
+	```bash
+	modules$ python main.py --holdout --n_samples 500 --n_repeats 3 --n_jobs 4
+	Creating data...
+	Evaluating...
+	                         mean    std  n_scores
+	test_roc_auc            0.892  0.035      15.0
+	test_average_precision  0.866  0.051      15.0
+	test_neg_log_loss      -0.425  0.075      15.0
+	test_accuracy           0.827  0.036      15.0
+	test_recall             0.840  0.059      15.0
+	test_precision          0.817  0.043      15.0
+	test_f1                 0.827  0.038      15.0
+	test_f1_weighted        0.826  0.036      15.0
+	
+	Checking holdout...
+	roc_auc_score              0.940000
+	average_precision_score    0.935374
+	neg_log_loss              -0.322117
+	accuracy_score             0.860000
+	recall_score               0.820000
+	precision_score            0.891304
+	f1_score                   0.854167
+	f1_score_weighted          0.859776
+	```
+
+	
+	
---
+++ modules/main.py
@@ -0,0 +1,192 @@
+from dataclasses import dataclass, field
+from typing import List
+
+import argparse
+
+import sklearn as sk
+from sklearn.model_selection import RepeatedStratifiedKFold, cross_validate
+from sklearn.metrics import (average_precision_score,
+                             roc_auc_score, recall_score,
+                             precision_score, accuracy_score,
+                             f1_score, accuracy_score, log_loss)
+
+
+# https://realpython.com/python-data-classes/
+@dataclass
+class Evaluator:
+
+    """Evaluate X, y, returning scores in self.scores_
+
+    Optionally, you can predict (and evaluate) on an additional holdout dataset
+
+    """
+    estimator: sk.base.BaseEstimator
+    # setting a default list with a dataclass is not so intuitive
+    score_metrics: List[str] = field(
+        default_factory=lambda: ["roc_auc",
+                                 "average_precision",
+                                 "neg_log_loss",
+                                 "accuracy", "recall",
+                                 "precision", "f1",
+                                 "f1_weighted"])
+    outer_cv: int = 5
+    n_repeats: int = 1
+    n_jobs: int = 1
+    random_state: int = 1
+
+    def train_evaluate(self, X, y, summarise_scores=True):
+        # (are these assertions pythonic?)
+        assert type(
+            self.score_metrics) == list, "score_metrics must be a List[str]"
+        assert all([x in sk.metrics.SCORERS.keys() for
+                    x in self.score_metrics]), "all score metrics must match sk.metrics.SCORERS.keys()"
+
+        rkf = RepeatedStratifiedKFold(n_splits=self.outer_cv,
+                                      n_repeats=self.n_repeats,
+                                      random_state=self.random_state)
+        scores_dict = cross_validate(self.estimator, X, y, cv=rkf,
+                                     scoring=self.score_metrics,
+                                     n_jobs=self.n_jobs)
+
+        if summarise_scores:
+            self.scores_ = self.summarise_scores(scores_dict)
+        else:
+            self.scores_ = scores_dict
+
+    def train_predict(self, X, y, X_new, y_new=None):
+        self.estimator_ = self.estimator.fit(X, y)
+        # uses estimator_ not estimator
+        self.preds_ = self.estimator_.predict(X_new)
+        self.preds_prob_ = self.estimator_.predict_proba(X_new)[:, 1]
+
+        if y_new is not None:
+            self.holdout_scores_ = self._compute_holdout_scores(y_new)
+
+    def summarise_scores(self, scores: dict, round_places: int = 3) -> dict:
+        """scores is a result from cross_validate()"""
+        metric_names = [x for x in scores.keys() if "test_" in x]
+        summary = dict()
+        for metric in metric_names:
+            scores_for_this_metric = scores[metric]
+            summary[metric] = {
+                'mean': scores_for_this_metric.mean().round(round_places),
+                'std': scores_for_this_metric.std().round(round_places),
+                'n_scores': len(scores_for_this_metric)
+            }
+        return(summary)
+
+    def _compute_holdout_scores(self, y_new) -> dict:
+        return dict(
+            # use preds_prob_
+            roc_auc_score=roc_auc_score(y_new, self.preds_prob_),
+            average_precision_score=average_precision_score(
+                y_new, self.preds_prob_),
+            neg_log_loss=-log_loss(y_new, self.preds_prob_),
+            # use preds_
+            accuracy_score=accuracy_score(y_new, self.preds_),
+            recall_score=recall_score(y_new, self.preds_),
+            precision_score=precision_score(y_new, self.preds_),
+            f1_score=f1_score(y_new, self.preds_),
+            f1_score_weighted=f1_score(
+                y_new, self.preds_, average="weighted")
+        )
+
+
+if __name__ == '__main__':
+    import pandas as pd
+
+    from sklearn.datasets import make_classification
+    from sklearn.pipeline import make_pipeline
+    from sklearn.decomposition import PCA
+    from sklearn.preprocessing import StandardScaler
+    from sklearn.model_selection import train_test_split
+    from sklearn.linear_model import LogisticRegressionCV
+
+    parser = argparse.ArgumentParser(description="Arguments")
+    parser.add_argument('--imbalanced', action="store_true",
+                        help="""Create imbalanced data?""")
+    parser.add_argument('--holdout', action="store_true",
+                        help="""Withold some data for a holdout evaluation?""")
+    parser.add_argument('--clf', default='lrcv', nargs="+", choices=['lrcv', 'gbm', 'hgbm'],
+                        help="""Classifier: any of lrcv, gbm, hgbm""")
+    parser.add_argument('--n_samples', default=100,
+                        help="""Number of samples in data (includes --holdout). Default is 300.""")
+    parser.add_argument('--n_features', default=20,
+                        help="""Number of features in data. Default is 20.""")
+    parser.add_argument('--n_informative', default=16,
+                        help="""Number of informative features in data. Default is 16.""")
+    parser.add_argument('--n_repeats', default=1,
+                        help="""Number of repeats for nested CV. Default is 1.""")
+    parser.add_argument('--add_pca', action="store_true",
+                        help="""Add PCA?""")
+    parser.add_argument('--n_jobs', default=2,
+                        help="""Number of cores to use. Default is 2.""")
+    parser.add_argument('--random_state', default=99,
+                        help="""Seed for sklearn""")
+
+    args = parser.parse_args()
+
+    imbalanced = args.imbalanced
+    holdout = args.holdout
+    clf = args.clf              # TODO implement different kinds
+    n_samples = int(args.n_samples)
+    n_features = int(args.n_features)
+    n_informative = int(args.n_informative)
+    n_repeats = int(args.n_repeats)
+    add_pca = args.add_pca
+    n_jobs = int(args.n_jobs)
+    random_state = int(args.random_state)
+
+    print("Creating data...")
+    weights = None
+    if imbalanced:
+        weights = [1.9,  0.8]
+
+    X_, y_ = make_classification(n_samples=n_samples, n_features=20,
+                                 n_informative=18, n_classes=2,
+                                 weights=weights, random_state=random_state)
+
+    if holdout:
+        holdout_size = 0.2
+        X, X_holdout, y, y_holdout = train_test_split(
+            X_, y_, test_size=holdout_size, random_state=random_state)
+    else:
+        X = X_
+        y = y_
+
+    # TODO convert into arguments
+    n_components = 0.95
+    outer_cv = 5
+    inner_cv = 5
+    max_iter = 300
+    class_weight = None
+
+    inner_scoring = "roc_auc"
+
+    lrcv = LogisticRegressionCV(cv=inner_cv, scoring=inner_scoring,
+                                n_jobs=1,
+                                class_weight=class_weight,
+                                random_state=random_state, max_iter=max_iter)
+
+    if add_pca:
+        pipe = make_pipeline(
+            StandardScaler(),
+            PCA(n_components=n_components, random_state=random_state),
+            lrcv
+        )
+    else:
+        pipe = make_pipeline(
+            StandardScaler(),
+            lrcv
+        )
+
+    print("Evaluating...")
+    ev = Evaluator(estimator=pipe, outer_cv=outer_cv, n_repeats=n_repeats,
+                   n_jobs=n_jobs, random_state=random_state)
+    ev.train_evaluate(X, y, summarise_scores=True)
+    print(pd.DataFrame(ev.scores_).T)
+
+    if holdout:
+        print("\nChecking holdout...")
+        ev.train_predict(X, y, X_holdout, y_holdout)
+        print(pd.Series(ev.holdout_scores_))
